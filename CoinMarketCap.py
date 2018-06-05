21b509f8ec8ce7b3a94801fed123c5d02bb1a1d6
fieryfury3 <39202955+fieryfury3@users.noreply.github.com>
2018-06-05 09:19:56 -0500

    Added ability to show info about all Top 100 coins

--- CoinMarketCap.py
+++ CoinMarketCap.py
@@ -1,6 +1,6 @@
 import locale
 import requests
-locale.setlocale( locale.LC_ALL, '')
+locale.setlocale(locale.LC_ALL, '')
 
 
 url = "https://api.coinmarketcap.com/v2/ticker/"
@@ -21,7 +21,7 @@ def searchanother():
 
 
 def search():
-    print("Please type name or symbol to view a single coin. Type 1h, 24h, or 7d to view coins with a 5% or greater increase respectively.")
+    print("Please type name or symbol to view a single coin or 'All' to view Top 100. Type 1h, 24h, 7d for price increases above 5% for relative time.")
     coin = input().lower()  # input of user, forced to lowercase to match coin_name
     for id in data_json.values():
         coin_name = id["name"].lower()  # name in coinmarketcap database
@@ -30,30 +30,47 @@ def search():
         hour_1_change = id["quotes"]["USD"]["percent_change_1h"]
         day_7_change = id["quotes"]["USD"]["percent_change_7d"]
 
-        if coin == "24h" and hour_24_change >= 5:   # prints name and percentage if the coin has increased in value more than 5% in the past 24 hours
+        if coin == "all":
+            for i in range(1):
+                print("Name: " + id["name"])
+                print("Symbol: " + id["symbol"])
+                print("Price: " + locale.currency(id["quotes"]["USD"]["price"], grouping=True))
+                print("Market Cap: " + locale.currency(id["quotes"]["USD"]["market_cap"], grouping=True))
+                print("Circulating Supply: " + '{:,.0f}'.format(int((id["circulating_supply"]))))
+                print("Total Supply: " + '{:,.0f}'.format(int((id["total_supply"]))))
+                print("1 hour change: " + str(hour_1_change) + "%")
+                print("24 hour change: " + str(hour_24_change) + "%")
+                print("7 day change: " + str(day_7_change) + "%")
+                print("24h Volume: " + "$" + '{:,.0f}'.format(int(id["quotes"]["USD"]["volume_24h"])))
+                print("\n\n\n")
+
+
+
+        elif coin == "24h" and hour_24_change >= 5:   # prints name and percentage if the coin has increased in value more than 5% in the past 24 hours
             print(coin_name.title())
-            print("24 hour change: " + str(id["quotes"]["USD"]["percent_change_24h"]) + "%" + "\n")
+            print("24 hour change: " + str(hour_24_change) + "%" + "\n")
 
-        if coin == "1h" and hour_1_change >= 5:   # prints name and percentage if the coin has increased in value more than 5% in the past hour
+        elif coin == "1h" and hour_1_change >= 5:   # prints name and percentage if the coin has increased in value more than 5% in the past hour
             print(coin_name.title())
-            print("1 hour change: " + str(id["quotes"]["USD"]["percent_change_1h"]) + "%" + "\n")
-
-        if coin == "7d" and day_7_change >= 5:   # prints name and percentage if the coin has increased in value more than 5% in the past 7 days
-            print(coin_name.title())
-            print("7 day change: " + str(id["quotes"]["USD"]["percent_change_7d"]) + "%" + "\n")
+            print("1 hour change: " + str(hour_1_change) + "%" + "\n")
 
 
+        elif day_7_change != None and coin == "7d" and day_7_change >= 5:   # prints name and percentage if the coin has increased in value more than 5% in the past 7 days
+                print(coin_name.title())
+                print("7 day change: " + str(day_7_change) + "%" + "\n")
 
 
-        if coin_name == coin:
+
+        elif coin_name == coin:
             print("Name: " + id["name"])
             print("Symbol: " + id["symbol"])
             print("Price: " + locale.currency(id["quotes"]["USD"]["price"], grouping=True))
             print("Market Cap: " + locale.currency(id["quotes"]["USD"]["market_cap"], grouping=True))
-            print("Total Supply: " + '{:,.0f}'.format(int((id["total_supply"])))) # Better way to format?
-            print("1 hour change: " + str(id["quotes"]["USD"]["percent_change_1h"]) + "%")
-            print("24 hour change: " + str(id["quotes"]["USD"]["percent_change_24h"]) + "%")
-            print("7 day change: " + str(id["quotes"]["USD"]["percent_change_7d"]) + "%")
+            print("Circulating Supply: " + '{:,.0f}'.format(int((id["circulating_supply"]))))
+            print("Total Supply: " + '{:,.0f}'.format(int((id["total_supply"]))))
+            print("1 hour change: " + str(hour_1_change) + "%")
+            print("24 hour change: " + str(hour_24_change) + "%")
+            print("7 day change: " + str(day_7_change) + "%")
             print("24h Volume: " + "$" + '{:,.0f}'.format(int(id["quotes"]["USD"]["volume_24h"])))
             searchanother()
 
@@ -62,10 +79,11 @@ def search():
             print("Symbol: " + id["symbol"])
             print("Price: " + locale.currency(id["quotes"]["USD"]["price"], grouping=True))
             print("Market Cap: " + locale.currency(id["quotes"]["USD"]["market_cap"], grouping=True))
-            print("Total Supply: " + '{:,.0f}'.format(int((id["total_supply"]))))  # Better way to format?
-            print("1 hour change: " + str(id["quotes"]["USD"]["percent_change_1h"]) + "%")
-            print("24 hour change: " + str(id["quotes"]["USD"]["percent_change_24h"]) + "%")
-            print("7 day change: " + str(id["quotes"]["USD"]["percent_change_7d"]) + "%")
+            print("Circulating Supply: " + '{:,.0f}'.format(int((id["circulating_supply"]))))
+            print("Total Supply: " + '{:,.0f}'.format(int((id["total_supply"]))))
+            print("1 hour change: " + str(hour_1_change) + "%")
+            print("24 hour change: " + str(hour_24_change) + "%")
+            print("7 day change: " + str(day_7_change) + "%")
             print("24h Volume: " + "$" + '{:,.0f}'.format(int(id["quotes"]["USD"]["volume_24h"])))
             searchanother()
 
